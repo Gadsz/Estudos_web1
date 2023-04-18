d1ee4c00a191d4a92937c7c0504851ce2913688b
Gabriel Albuquerque da Silva <82517993+Gadsz@users.noreply.github.com>
2023-04-18 15:56:56 -0300

    Add files via upload

---
+++ mobile-navbar.js
@@ -0,0 +1,26 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.MobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+    }
+    
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log ("Hey 🦊"));
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile.menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
