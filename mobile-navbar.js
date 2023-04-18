baa77ba61d53b367ec199df144fda7408b5a0cf8
Gabriel Albuquerque da Silva <82517993+Gadsz@users.noreply.github.com>
2023-04-18 17:31:53 -0300

    Add files via upload

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,25 +1,27 @@
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(this.mobileMenu);
-        this.navList = document.querySelector(this.navList);
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
 
-        this.handleClick = this.handleClick.bind();
+        this.handleClick = this.handleClick.bind(this);
     }
 
     animateLinks() {
         this.navLinks.forEach((link, index) => {
             link.style.animation
               ? (link.style.animation = "")
-              : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+              : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+            }s`);
         });
     }
 
     handleClick() {
         this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
-        this.animatedLinks();
+        this.animateLinks();
     }
 
     addClickEvent() {
